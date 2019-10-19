4c000c96d35b0abe06e9f6b31bd9c361fd9dbeb3
brianabajah <abajahbrian@gmail.com>
2019-10-19 18:26:26 -0400

    lost files

---
+++ ERP/src/main/java/com/pro1/dtbAccessors/Authentication.java
@@ -0,0 +1,8 @@
+package com.pro1.dtbAccessors;
+
+public class Authentication {
+	private String url= "";
+	private String name= "";
+	private String pass= "";
+	
+}
---
+++ ERP/src/main/java/com/pro1/reimburse/users/UserRole.java
@@ -0,0 +1,35 @@
+package com.pro1.reimburse.users;
+
+public class UserRole {
+private int role_id;
+private String role;
+
+
+
+
+public UserRole(int role_id, String role) {
+	super();
+	this.role_id = role_id;
+	this.role = role;
+}
+
+public UserRole(int role_id) {
+	super();
+	this.role_id =role_id;
+}
+
+public int getRole_id() {
+	return role_id;
+}
+public void setRole_id(int role_id) {
+	this.role_id = role_id;
+}
+public String getRole() {
+	return role;
+}
+public void setRole(String role) {
+	this.role = role;
+}
+
+
+}
